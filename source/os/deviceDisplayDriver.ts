524909bc4af6f68437e004e5dbd551115936fb19
kevinmirsky <kevinmirsky@gmail.com>
2018-10-13 22:01:43 -0400

    Perform style pass on UI 😎

---
+++ source/os/deviceDisplayDriver.ts
@@ -0,0 +1,15 @@
+module TSOS {
+    /*
+    This class is meant to control the display of various system
+     */
+    export class deviceDisplayDriver extends DeviceDriver {
+
+        public displayPcb(): void {
+
+        }
+
+        public displayMemory(): void {
+            //We'll migrate the code here later
+        }
+    }
+}
